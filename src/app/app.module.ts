08ee88b81477e58f6221066b00649aa8549831b4
Bereczki Attila <bereczki.attila@ipsystems.hu>
2017-12-22 14:21:48 +0100

    Komponenesekre bontás

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CollapseModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    JumbotronComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
