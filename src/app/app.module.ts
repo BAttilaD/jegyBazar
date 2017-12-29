ec05188f92186345e1eda66855f2520247fb6177
Bereczki Attila <bereczki.attila@ipsystems.hu>
2017-12-30 00:04:01 +0100

    Basci component generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,21 @@ import { NavComponent } from './nav/nav.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
 import { FooterComponent } from './footer/footer.component';
 import { EventcardComponent } from './eventcard/eventcard.component';
+import { EventComponent } from './event/event.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileEditComponent } from './profile-edit/profile-edit.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { BidComponent } from './bid/bid.component';
+import { TicketDetailComponent } from './ticket-detail/ticket-detail.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -16,11 +31,14 @@ import { EventcardComponent } from './eventcard/eventcard.component';
     NavComponent,
     JumbotronComponent,
     FooterComponent,
-    EventcardComponent
+    EventcardComponent,
+    EventComponent,
+    ...AppRoutingModule.routableComponents
   ],
   imports: [
     BrowserModule,
-    CollapseModule
+    CollapseModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
