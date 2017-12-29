ad441871f2ccfa70e06fe8186c0c57218ec47835
Bereczki Attila <bereczki.attila@ipsystems.hu>
2017-12-29 22:37:33 +0100

    Revert "Komponenesk keszitese az uj oldalakbol"
    This reverts commit d7c9a64f86fd24e2c65d1c14163ab24c725d8140.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CollapseModule, AlertModule  } from 'ngx-bootstrap';
+import { CollapseModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -8,11 +8,6 @@ import { NavComponent } from './nav/nav.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
 import { FooterComponent } from './footer/footer.component';
 import { EventcardComponent } from './eventcard/eventcard.component';
-import { EsemenyekComponent } from './esemenyek/esemenyek.component';
-import { JegyekComponent } from './jegyek/jegyek.component';
-import { RolunkComponent } from './rolunk/rolunk.component';
-import { BelepesComponent } from './belepes/belepes.component';
-import { RegisztracioComponent } from './regisztracio/regisztracio.component';
 
 
 @NgModule({
@@ -21,15 +16,9 @@ import { RegisztracioComponent } from './regisztracio/regisztracio.component';
     NavComponent,
     JumbotronComponent,
     FooterComponent,
-    EventcardComponent,
-    EsemenyekComponent,
-    JegyekComponent,
-    RolunkComponent,
-    BelepesComponent,
-    RegisztracioComponent
+    EventcardComponent
   ],
   imports: [
-    AlertModule.forRoot(),
     BrowserModule,
     CollapseModule
   ],
