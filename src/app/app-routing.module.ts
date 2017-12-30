ffe07ebe2d790f63b7d8420fbeb5f04dbfadf446
Bereczki Attila <bereczki.attila@ipsystems.hu>
2017-12-30 22:42:35 +0100

    Refactor a mappaszerkezet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
-import { TicketDetailComponent } from './ticket-detail/ticket-detail.component';
-import { ProfileEditComponent } from './profile-edit/profile-edit.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { TicketDetailComponent } from './ticket/ticket-detail/ticket-detail.component';
+import { ProfileEditComponent } from './user/profile-edit/profile-edit.component';
+import { RegistrationComponent } from './user/registration/registration.component';
 import { AboutComponent } from './about/about.component';
 import { EventComponent } from './event/event.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import { TicketComponent } from './ticket/ticket.component';
-import { LoginComponent } from './login/login.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { EventDetailComponent } from './event-detail/event-detail.component';
-import { ProfileComponent } from './profile/profile.component';
-import { TicketListComponent } from './ticket-list/ticket-list.component';
+import { LoginComponent } from './user/login/login.component';
+import { EventListComponent } from './event/event-list/event-list.component';
+import { EventDetailComponent } from './event/event-detail/event-detail.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { TicketListComponent } from './ticket/ticket-list/ticket-list.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -22,7 +22,7 @@ const routes: Routes = [
         children: [
             { path: 'list', component: EventListComponent },
             { path: 'new', component: EventDetailComponent },
-            { path: ':id/edit', component: EventDetailComponent }
+            { path: ':id', component: EventDetailComponent }
         ]
     },
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,11 @@ import { CollapseModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { JumbotronComponent } from './jumbotron/jumbotron.component';
-import { FooterComponent } from './footer/footer.component';
-import { EventcardComponent } from './eventcard/eventcard.component';
+import { NavComponent } from './core/nav/nav.component';
+import { JumbotronComponent } from './core/jumbotron/jumbotron.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { EventcardComponent } from './event/eventcard/eventcard.component';
 import { EventComponent } from './event/event.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ProfileEditComponent } from './profile-edit/profile-edit.component';
-import { EventDetailComponent } from './event-detail/event-detail.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { BidComponent } from './bid/bid.component';
-import { TicketDetailComponent } from './ticket-detail/ticket-detail.component';
-import { TicketListComponent } from './ticket-list/ticket-list.component';
-import { TicketComponent } from './ticket/ticket.component';
 import { AppRoutingModule } from './app-routing.module';
 
 
